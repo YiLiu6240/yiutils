0d00d80c6aff9733d1d1e6aa3be4b017c90eea64
YiLiu6240 <yi.liu6240@gmail.com>
2023-07-09 20:45:15 +0100

    Init pkg

---
+++ scripts/sanity.py
@@ -0,0 +1,8 @@
+from yiutils.proj_paths import find_project_root
+
+def main():
+    root = find_project_root(anchor_file="environment.yml")
+    print(root)
+
+if __name__ == "__main__":
+    main()
