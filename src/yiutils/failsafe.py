2ba5a8a9568af48e2a8671293c9270b9ee1fad14
YiLiu6240 <yi.liu6240@gmail.com>
2023-07-10 16:32:24 +0100

    wip failsafe

--- src/yiutils/failsafe.py
+++ src/yiutils/failsafe.py
@@ -4,7 +4,8 @@ from typing import Callable, Optional, Any, Union, Tuple
 from inspect import getcallargs
 
 _CONTINGENT_ERROR = Union[bool, Exception]
-_FAILSAFE_RES = Tuple[Optional[Any], _CONTINGENT_ERROR]
+_CONTEXT = Optional[Any]
+_FAILSAFE_RES = Tuple[Optional[Any], _CONTINGENT_ERROR, _CONTEXT]
 
 def failsafe(f_py: Optional[Callable] = None, silent: bool = False) -> _FAILSAFE_RES:
     # https://stackoverflow.com/a/60832711
@@ -14,16 +15,18 @@ def failsafe(f_py: Optional[Callable] = None, silent: bool = False) -> _FAILSAFE
             try:
                 func_res = func(*args, **kwargs)
                 contingent_error = True
+                context = None
             except Exception as error:
                 func_res = None
                 contingent_error = error
+                context = getcallargs(func, *args, **kwargs)
                 if not silent:
                     message = "failsafe issue detected: error: {error}; context: {context}".format(
                         error="{type} -- {error}".format(type=type(error).__name__, error=error),
-                        context=getcallargs(func, *args, **kwargs)
+                        context=context
                     )
                     warnings.warn(message)
-            wrapper_res: _FAILSAFE_RES = (func_res, contingent_error)
+            wrapper_res: _FAILSAFE_RES = (func_res, contingent_error, context)
             return wrapper_res
         return wrapper
     failsafe_res = _failsafe(f_py) if callable(f_py) else _failsafe
---
+++ tests/test_failsafe.py
@@ -0,0 +1,27 @@
+from yiutils.failsafe import failsafe
+
+@failsafe
+def good():
+    return 1
+
+@failsafe
+def bad(x: int = 0):
+    y = 12 / 0
+    return x
+
+def test_good():
+    res = good()
+    assert isinstance(res, tuple)
+    assert len(res) == 3
+    assert res == (1, True, None)
+
+def test_bad():
+    res = bad()
+    assert isinstance(res, tuple)
+    assert len(res) == 3
+    assert res[0] == None
+    assert isinstance(
+        res[1],
+        ZeroDivisionError
+    )
+    assert res[2] == {"x": 0}
