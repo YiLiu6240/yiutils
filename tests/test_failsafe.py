19f3eb4e37818e56dd4554b279f0f6b1db3768d6
YiLiu6240 <yi.liu6240@gmail.com>
2023-07-10 17:01:02 +0100

    Clean up code

--- tests/test_failsafe.py
+++ tests/test_failsafe.py
@@ -1,13 +1,24 @@
+import pytest
+
 from yiutils.failsafe import failsafe
+
 
 @failsafe
 def good():
     return 1
 
+
 @failsafe
 def bad(x: int = 0):
-    y = 12 / 0
+    y = 12 / 0  # noqa
     return x
+
+
+@failsafe(silent=True)
+def silent_bad(x: int = 0):
+    y = 12 / 0  # noqa
+    return x
+
 
 def test_good():
     res = good()
@@ -15,13 +26,22 @@ def test_good():
     assert len(res) == 3
     assert res == (1, True, None)
 
+
 def test_bad():
-    res = bad()
+    with pytest.warns(match="division by zero") as record:
+        res = bad()
     assert isinstance(res, tuple)
     assert len(res) == 3
-    assert res[0] == None
-    assert isinstance(
-        res[1],
-        ZeroDivisionError
-    )
+    assert res[0] is None
+    assert isinstance(res[1], ZeroDivisionError)
+    assert res[2] == {"x": 0}
+    assert len(record) == 1
+
+
+def test_silient_bad():
+    res = silent_bad()
+    assert isinstance(res, tuple)
+    assert len(res) == 3
+    assert res[0] is None
+    assert isinstance(res[1], ZeroDivisionError)
     assert res[2] == {"x": 0}
