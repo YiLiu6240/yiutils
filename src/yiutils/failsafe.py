586e4a645d95acd34c9e9d92527cfbee8141f67b
YiLiu6240 <yi.liu6240@gmail.com>
2023-07-10 16:13:12 +0100

    wip failsafe

--- src/yiutils/failsafe.py
+++ src/yiutils/failsafe.py
@@ -1,6 +1,7 @@
-from typing import Callable, Optional, Any, Union
-
 import functools
+import warnings
+from typing import Callable, Optional, Any, Union, Tuple
+from inspect import getcallargs
 
 _CONTINGENT_ERROR = Union[bool, Exception]
 _FAILSAFE_RES = Tuple[Optional[Any], _CONTINGENT_ERROR]
@@ -10,15 +11,19 @@ def failsafe(f_py: Optional[Callable] = None, silent: bool = False) -> _FAILSAFE
     def _failsafe(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs) -> _FAILSAFE_RES:
-            wrapper_res: _FAILSAFE_RES
-            error: _CONTINGENT_ERROR
             try:
                 func_res = func(*args, **kwargs)
                 contingent_error = True
             except Exception as error:
                 func_res = None
                 contingent_error = error
-            wrapper_res = (func_res, error)
+                if not silent:
+                    message = "failsafe issue detected: error: {error}; context: {context}".format(
+                        error="{type} -- {error}".format(type=type(error).__name__, error=error),
+                        context=getcallargs(func, *args, **kwargs)
+                    )
+                    warnings.warn(message)
+            wrapper_res: _FAILSAFE_RES = (func_res, contingent_error)
             return wrapper_res
         return wrapper
     failsafe_res = _failsafe(f_py) if callable(f_py) else _failsafe
