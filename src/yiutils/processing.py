15baa14e90e68a9a5c170f1f3e72b20a1c782101
YiLiu6240 <yi.liu6240@gmail.com>
2023-07-26 22:09:03 +0100

    processing_wrapper: Add prefix

--- src/yiutils/processing.py
+++ src/yiutils/processing.py
@@ -9,11 +9,14 @@ def processing_wrapper(
     payload: Dict[str, Any],
     func: Callable,
     echo_step: int = 0,
+    prefix: str = "",
 ) -> Any:
+    if len(prefix) != 0:
+        prefix = prefix + "\t"
     # Auto decide echo_step
     if echo_step == 0:
         echo_step = round(total / 5)
     if idx % echo_step == 0:
-        logger.info(f"#{idx}/{total}, payload: {payload}")
+        logger.info(f"{prefix}#{idx}/{total}, payload: {payload}")
     res = func(**payload)
     return res
